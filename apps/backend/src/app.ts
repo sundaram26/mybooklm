955f733f2f6fd470fd5bd25ac8dd52e675b26122
sundaram26 <singhsundaram2004@gmail.com>
2026-07-28 06:26:57 +0530

    cors error

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -17,7 +17,7 @@ import { createRateLimiter } from "./api/middlewares/rate-limit.middleware";
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000", "http://192.168.0.105:3000"],
+    origin: ["http://localhost:3000", "https://noetalm.sundaramsingh.com"],
     credentials: true,
 }));
 app.use(json());
